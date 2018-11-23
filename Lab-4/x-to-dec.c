11a9975f8a7fe49aeb99607ae540c2fccc91be56
Alex Calugari <afishr00@gmail.com>
2018-11-23 19:44:30 +0200

    Fix B1E2

--- Lab-4/x-to-dec.c
+++ Lab-4/x-to-dec.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 int main()
 {
 
-	long int init, decimal = 0;
-	int i = 0, base;
+	long int decimal = 0;
+	int base, temp;
+	char* init = malloc(100);
 
-	scanf("%ld %d", &init, &base);
-	while (init != 0)
+	scanf("%s", init);
+	scanf("%d", &base);
+
+
+	int len = (int)(strlen(init)) - 1;
+
+	for (int i = 0; init[i] != '\0'; i++)
 	{
-		decimal += (init % 10) * pow(base, i++);
-		init /= 10;
+		if(init[i] >= '0' && init[i] <= '9')
+		{
+			temp = init[i] - 48;
+		}
+
+		else if(init[i] >= 'A' && init[i] <= 'F')
+		{
+			temp = init[i] - 55;
+		}
+
+		decimal += (temp) * pow(base, len--);
 	}
 	printf("%ld",decimal);
+
+	free(init);
 	return 0;
 }
